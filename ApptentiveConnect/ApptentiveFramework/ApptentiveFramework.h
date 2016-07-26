e04a11aee17ec40285e53367d2961bf256379a03
Nikita Leskin <lyeskin@gmail.com>
2016-07-26 11:07:41 +0300

    Configured dynamic framework

---
+++ ApptentiveConnect/ApptentiveFramework/ApptentiveFramework.h
@@ -0,0 +1,19 @@
+//
+//  ApptentiveFramework.h
+//  ApptentiveFramework
+//
+//  Created by Nikita Leskin on 7/26/16.
+//  Copyright © 2016 Apptentive, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApptentiveFramework.
+FOUNDATION_EXPORT double ApptentiveFrameworkVersionNumber;
+
+//! Project version string for ApptentiveFramework.
+FOUNDATION_EXPORT const unsigned char ApptentiveFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApptentiveFramework/PublicHeader.h>
+
+#import <ApptentiveFramework/ATConnect.h>
